8960250fa34d777407855f61aa796069124f541b
FlaviuVadan <flaviuvadan@gmail.com>
2019-01-26 18:27:37 -0600

    Add a simple email validation regex (note, it accepts ..domain)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
 	# note, there is a difference between a class instance variable and a class variable
 	# in this case, name_limit and email_limit are class instance variables and are only accessible to an instance of a
 	# User but not by descendants of the User class
-	@name_limit  = 50
-	@email_limit = 255
+	name_limit  = 50
+	email_limit = 255
+	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	validates :name, presence: true, length: { maximum: @name_limit }
-	validates :email, presence: true, length: { maximum: @email_limit }
+	validates :name, presence: true, length: { maximum: name_limit }
+	validates :email, presence: true, length: { maximum: email_limit }, format: { with: valid_email_regex }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,11 +21,28 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "name should not exceed limit" do
-	  @user.name = "a" * 51
+		@user.name = "a" * 51
 		assert_not @user.valid?
 	end
 
 	test "email should not exceed limit" do
 		@user.email = "a" * 244 + "@example.com"
+	end
+
+	test "email validation should accept valid addresses" do
+		# %w[] - a technique for making arrays of strings
+	  valid_addresses = %w[user@example.com USER@foo.COM A_US_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+		  @user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
+
+	test "email validation should reject invalid addresses" do
+	  invalid_addresses = %w[user@example,com USER_foo.com A_US_US-ER_at_foo.bar.org first.last_at_foo.jp foo@bar+baz.com]
+		invalid_addresses.each do |invalid_address|
+		  @user.email = invalid_address
+			assert_not @user.valid?, "#{invalid_address} should be invalid"
+		end
 	end
 end
