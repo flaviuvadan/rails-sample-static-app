42092db733a4dfe2769f36c6a31d508b88e5087d
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-02 17:43:57 -0600

    Add valid signup information test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		assert_template 'users/new'
 		assert_template 'shared/_error_messages'
 	end
+
+	test "Valid signup information" do
+		get signup_path
+		assert_difference 'User.count', 1 do
+			post users_path, params: {
+					user: {
+							name:                  "Test User",
+							email:                 "user@example.com",
+							password:              "password",
+							password_confirmation: "password"
+					},
+			}
+		end
+		follow_redirect!
+		assert_template 'users/show'
+		assert_select 'div.alert-success'
+	end
 end
