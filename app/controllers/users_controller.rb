d33e0a274a4f7a90b09c3d06bd48fe16bb91b3fc
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-03 12:01:25 -0600

    Store current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def current_user
+		if session[:user_id]
+			# @current_user = @current_user || User.find_by(id: session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
+
 	private
 
 		def user_params
