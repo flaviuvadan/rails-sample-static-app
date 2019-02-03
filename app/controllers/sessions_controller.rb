cec01d5c204163f00ea5e72b097830f7f81da57b
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-03 11:54:36 -0600

    Globally expose SessionHelper and complete login worflow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(email: params[:session][:email].downcase)
-		if @user && @user.authenticate(params[:session][:password])
-			# log in
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in(user)
+			redirect_to user # equivalent to user_url(user)
 		else
 			flash.now[:danger] = 'Invalid email/password combination'
 			render 'new'
