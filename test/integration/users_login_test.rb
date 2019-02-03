2e076070d3e23beaa32bdfa9019e0335a2a26971
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-03 12:26:00 -0600

    Add signup test and log in users upon signup/user creation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,6 +22,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", user_path(@user)
 	end
 
+	test "valid signup information" do
+		get signup_path
+		assert_difference "User.count", 1 do
+			post users_path, params: {
+					user: {
+							name:                  "Test User",
+							email:                 "test@user.com",
+							password:              "password",
+							password_confirmation: "password",
+					},
+			}
+		end
+		follow_redirect!
+		assert_template 'users/show'
+		assert is_logged_in?
+	end
+
 	test "login with invalid information" do
 		get login_path
 		assert_template 'sessions/new'
