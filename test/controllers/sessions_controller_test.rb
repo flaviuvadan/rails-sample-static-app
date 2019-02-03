64df0f2b59245e7ac49f1bf3833f6b95fc60884f
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-03 11:05:40 -0600

    Add sessions basic tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "should perform get" do
+    get login_path
+    assert_response :success
+  end
+
+  test "should perform create" do
+    post login_path
+    assert_response :success
+  end
+
+  test "should perform delete" do
+    delete logout_path
     assert_response :success
   end
 
