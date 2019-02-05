8532b400c2bef238782d424b5f545465e4e5cc68
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-04 18:40:13 -0600

    Only log out if a user is actually logged in. Add tests to confirm workflow

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ApplicationRecord
 
 	# Check whether a user is authenticated based on the current remember token
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 
