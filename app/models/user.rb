8b6596fbd1ed652be47f20b844d33eaff3427a0b
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-04 18:12:24 -0600

    Add remember token to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	# note, there is a difference between a class instance variable and a class variable
 	# in this case, name_limit and email_limit are class instance variables and are only accessible to an instance of a
 	# User but not by descendants of the User class
@@ -26,5 +28,14 @@ class User < ApplicationRecord
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
+	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 end
