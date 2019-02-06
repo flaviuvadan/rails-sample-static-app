dbaaa6fb4480c264bacaa2386a29d7463fd20358
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-05 19:53:37 -0600

    Allow updates with nil passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,8 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password,
 						presence: true,
-						length:   { minimum: 6 }
+						length:   { minimum: 6 },
+						allow_nil: true
 
 	# Create an encrypted version of the given string
 	def User.digest(string)
