54a1c1103db9bcaa3ee256cd27c18893078f3688
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-05 19:45:08 -0600

    Test unsuccessful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:Starman)
+	end
+
+	test "unsuccessful edit" do
+	  get edit_user_path(@user)
+		assert_template 'users/edit'
+		patch user_path(@user), params: {
+				user: {
+						name: "",
+						email: "invalid",
+						password: "foo",
+						password_confirmation: "bar",
+				},
+		}
+		assert_template 'users/edit'
+	end
 end
