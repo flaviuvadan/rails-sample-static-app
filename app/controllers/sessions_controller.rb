92c9acbea65e85a06650c4f6b943849c7d68d31e
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-06 19:42:31 -0600

    Add comments to login/out functions and implement friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
+
+	# Create a new session controller
 	def new
 	end
 
+	# Create a session upon logging in
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			log_in user
 			remember user
 			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-			redirect_to user # equivalent to user_url(user)
+			redirect_back_or user # equivalent to redirect_back_or(user)
 		else
 			flash.now[:danger] = 'Invalid email/password combination'
 			render 'new'
 		end
 	end
 
+	# Destroy a session upon logging out
 	def destroy
 		log_out if logged_in?
 		redirect_to root_url
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,21 +1,25 @@
 module SessionsHelper
 
+	# Log in a user
 	def log_in(user)
 		session[:user_id] = user.id
 	end
 
+	# Remember a user's session info
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
+	# Forget a user's session info
 	def forget(user)
 		user.forget
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
 	end
 
+	# Get the currently logged in user
 	def current_user
 		if (user_id = session[:user_id])
 			@current_user ||= User.find_by(id: user_id)
@@ -28,17 +32,32 @@ module SessionsHelper
 		end
 	end
 
+	# Check if a user is logged in
 	def logged_in?
 		!current_user.nil?
 	end
 
+	# Check if the given user is the current user
 	def current_user?(user)
 		user == current_user
 	end
 
+	# Log out and delete session info
 	def log_out
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	# Redirects to stored location or falls back on default
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# Stores the URL trying to be accessed
+	def store_location
+		# store forward url on get requests to avoid case of PATCH or DELETE
+		session[:forwarding_url] = request.original_url if request.get?
 	end
 end
