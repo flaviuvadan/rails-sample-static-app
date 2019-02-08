e34fc76498d6962d4d797b62f14b52c612ad6ae8
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-07 19:47:54 -0600

    Allow access to users path

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,7 +31,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect index when not logged in" do
-    get user_path
+    get users_path
     assert_redirected_to login_url
   end
 end
