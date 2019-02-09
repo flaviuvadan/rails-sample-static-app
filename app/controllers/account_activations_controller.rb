be4a68297db3c9c6a7d35a03cc3fda76cb8f2ba1
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-09 12:04:03 -0600

    Add downcase_email and create_activation_digest functions

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,21 @@
 class AccountActivationsController < ApplicationController
+
+	attr_accessor :remember_token, :activation_token
+
+	before_save :downcase_email
+	before_create :create_activation_digest
+	validates :name, presence: true, length: { maximum: 50 }
+
+	private
+
+		# Convert email to lowercase before saving a user
+		def downcase_email
+			self.email = email.downcase
+		end
+
+		# Create and assign the activation token
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
