74517d474fd6b472e062b81cbe0409a2a6436ebf
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-08 20:32:17 -0600

    Add user partial

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,7 +6,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 	end
 
 	test "index includes pagination" do
-	  log_in_as(@user)
+		log_in_as(@user)
 		get users_path
 		assert_template 'users/index'
 		assert_select 'div.pagination'
