a7aad0f1314cf406ffd7ff8c7c0fcd15addd2ebd
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-08 20:46:07 -0600

    Add admin test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,37 +1,50 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-    @user = users(:Starman)
-    @user_2 = users(:Zaphod)
-  end
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+		@user       = users(:Starman)
+		@user_2     = users(:Zaphod)
+	end
 
-  test "should get new" do
-    get signup_path
-    assert_response :success
-    assert_select "title", "Sign up | #{@base_title}"
-  end
+	test "should get new" do
+		get signup_path
+		assert_response :success
+		assert_select "title", "Sign up | #{@base_title}"
+	end
 
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
+	test "should redirect edit when not logged in" do
+		get edit_user_path(@user)
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
 
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: {
-        user: {
-            name: @user.name,
-            email: @user.email,
-        },
-    }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
+	test "should redirect update when not logged in" do
+		patch user_path(@user), params: {
+				user: {
+						name:  @user.name,
+						email: @user.email,
+				},
+		}
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
 
-  test "should redirect index when not logged in" do
-    get users_path
-    assert_redirected_to login_url
-  end
+	test "should redirect index when not logged in" do
+		get users_path
+		assert_redirected_to login_url
+	end
+
+	test "should not allow the admin attribute to be edited via the web" do
+		log_in_as(@user_2)
+		assert_not @user_2.admin?
+		patch user_path(@user_2), params: {
+				user: {
+						password:              'password',
+						password_confirmation: 'password',
+						admin:                 'true',
+				},
+		}
+		assert_not @user_2.admin?
+	end
 end
