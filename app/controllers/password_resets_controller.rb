90a52f57b36d835f4eca4a33e9d4d1d8d9af849c
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-10 09:39:17 -0600

    Define user params on password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@ class PasswordResetsController < ApplicationController
 
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiry, only: [:edit, :update]
 
   def new
   end
@@ -23,7 +24,23 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "cannot be empty")
+      render 'edit'
+		elsif @user.update_attributes(user_params)
+			log_in(@user)
+			flash[:success] = "Password has been reset"
+			redirect_to @user
+    end
+  end
+
   private
+
+		# Defines allowed params
+		def user_params
+			params.require(:user).permit(:password, :password_confirmation)
+		end
 
     # Get the user submitting the password reset
     def get_user
@@ -34,6 +51,14 @@ class PasswordResetsController < ApplicationController
     def valid_user
       unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
         redirect_to root_url
+      end
+    end
+
+    # Check whether a reset token has expired
+    def check_expiry
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset expired"
+        redirect_to new_password_reset_url
       end
     end
 end
