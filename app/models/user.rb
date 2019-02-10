96c2730634666c90750c97f05bf29d67b52fba86
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-10 09:06:43 -0600

    Implement create_reset_digest and send_password_reset_email on user model and finish password_reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 
 	before_create :create_activation_digest
 	before_save :downcase_email
@@ -76,5 +76,16 @@ class User < ApplicationRecord
 	# Activate a user's account
 	def activate
 		update_columns(activated: true, activated_at: Time.zone.now)
+	end
+
+	# Set password reset attributes
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+	end
+
+	# Send password reset email
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
 	end
 end
