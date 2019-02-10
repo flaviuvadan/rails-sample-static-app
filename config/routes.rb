d0f4959c2c64b2ee3559d5bbc1fc24a267132d45
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-10 08:37:39 -0600

    Adjust routes to include password_resets resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-	get 'users/new'
-
 	root 'static_pages#home'
 
+	get 'users/new'
 	get '/home', to: 'static_pages#home'
-
 	get '/help', to: 'static_pages#help'
-
 	get '/about', to: 'static_pages#about'
-
 	get '/contact', to: 'static_pages#contact'
-
 	get '/signup', to: 'users#new'
-
 	get '/login', to: 'sessions#new'
 
 	post '/login', to: 'sessions#create'
@@ -25,6 +15,6 @@ Rails.application.routes.draw do
 	delete '/logout', to: 'sessions#destroy'
 
 	resources :users
-
 	resources :account_activations, only: [:edit]
+	resources :password_resets, only: [:new, :create, :edit, :update]
 end
