b7d1a7f95e3950efdf1e018f7ac1a0aacb1e3dde
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-11 18:17:51 -0600

    Add stub create and destroy to micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,14 @@
 class MicropostsController < ApplicationController
+
+	before_action :logged_in_user, only: [:create, :destroy]
+
+	# Create a micropost
+	def create
+
+	end
+
+	# Destroy a micropost
+	def destroy
+
+	end
 end
