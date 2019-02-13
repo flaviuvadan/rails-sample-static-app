92fe738295b59062daacfb71974b6ad17129e1b3
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-12 19:38:51 -0600

    Include picture upload testing in micropost interface testing

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 	  log_in_as(@user)
 		get root_path
 		assert_select 'div.pagination'
+		assert_select 'input[type=file]'
 
 		# invalid submission
 		assert_no_difference 'Micropost.count' do
@@ -22,10 +23,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
 		# valid submission
 		content = "content"
+		picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
 		assert_difference 'Micropost.count', 1 do
 		  post microposts_path, params: {
 					micropost: {
-							content: content
+							content: content,
+							picture: picture,
 					}
 			}
 		end
