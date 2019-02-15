7544ada771c8879e4c092f1f87f699dc1b3bc028
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-15 05:41:43 -0600

    Add route to create/destroy relationships (follow/unfollow)

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,13 @@ Rails.application.routes.draw do
 	resources :users
 	resources :users do
 		# responds to routes of user/id
-	  member do
+		member do
 			# allow to get user's following and user's followers
 			get :following, :followers
-	  end
+		end
 	end
 	resources :account_activations, only: [:edit]
 	resources :password_resets, only: [:new, :create, :edit, :update]
 	resources :microposts, only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
 end
