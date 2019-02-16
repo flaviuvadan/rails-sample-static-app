83bcf3adc15724cc77a9d95c6d24eeb1abaa1c32
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-15 19:19:09 -0600

    Add create/destroy stub methods and test for redirect

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,13 @@
 class RelationshipsController < ApplicationController
+	before_action :logged_in_user
+
+	# Create a Relationship entity
+	def create
+
+	end
+
+	# Delete a Relationship entity
+	def destroy
+
+	end
 end
