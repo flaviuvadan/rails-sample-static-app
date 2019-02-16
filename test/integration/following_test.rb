7e356b208ea60397641bb13d2fcf6c6f34a43626
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-16 15:18:34 -0600

    Add follow/unfollow tests

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@user = users(:Starman)
+		@user_2 = users(:Zaphod)
 		log_in_as(@user)
 	end
 
@@ -22,6 +23,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
 		assert_match @user.followers.count.to_s, response.body
 		@user.followers.each do |user|
 			assert_select "a[href=?]", user_path(user)
+		end
+	end
+
+	test "follow user non-ajax" do
+	  assert_difference '@user.following.count', 1 do
+	    post relationships_path, params: { followed_id: @user_2.id }
+	  end
+	end
+
+	test "follow user ajax" do
+		assert_difference '@user.following.count', 1 do
+			post relationships_path, xhr: true, params: { followed_id: @user_2.id }
+		end
+	end
+
+	test "unfollow users non-ajax" do
+	  @user.follow(@user_2)
+		relationship = @user.active_relationships.find_by(followed_id: @user_2.id)
+		assert_difference '@user.following.count', -1 do
+			delete relationship_path(relationship)
+		end
+	end
+
+	test "unfollow users ajax" do
+		@user.follow(@user_2)
+		relationship = @user.active_relationships.find_by(followed_id: @user_2.id)
+		assert_difference '@user.following.count', -1 do
+			delete relationship_path(relationship), xhr: true
 		end
 	end
 end
